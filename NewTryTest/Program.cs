1b0d97d5d6271b212b23fe4bf3c9e2ccde3f3031
luftwaffe1985 <putik1985@yandex.ru>
2022-10-04 18:45:57 +0300

    Added New Quarter Test

---
+++ NewTryTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NewTryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+// Написать программу, которая из имеющегося массива строк формирует 
+// массив из строк, длина которых либо меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма.
+// При решение не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+      
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "");
+    }
+    Console.WriteLine("]");
+}
+string[] array = { "door", "8", " = ", "yahoo", "OCT", "wild", "freeze", "Geek" };
+string[] newArray = new string[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3) newArray[i] = array[i];
+}
+PrintArray(newArray);
+        }
+    }
+}
